9c6bbee56174a79858d2bfa5b727ebae839f6284
Nikhil Soni <nikhilkamlesh.soni@gmail.com>
2018-10-12 16:34:21 -0400

    Problems and concepts added
    Problems: 6 to 10
    Concepts: 1 to 4

--- Project_1_Solution.py
+++ Project_1_Solution.py
@@ -63,4 +63,60 @@ print(data[9] == data[10])
 #   just means the Mth element from the starting of the list. Mth Element = M-1th Index
 
 
+#   Concept 1 : if-else conditional statement
+#   The following statements read as:
+#   1.1. "a == b ": Is a equal to b, returns either True or False
+#   1.2. "a != b": Is a not equal to b, returns either True or False
+#   1.3. "a in b": Whether a is contained in b
+#   if <A True Value is Encountered>:
+#       <Do this>
+#   else:
+#       <Do Something Else>
 
+#   Problem 6). Check whether "_RANDOM_STRING is contained in the list. Print the "Yes" if it is, otherwise print No.
+#   Hint: Refer the Concept 1: if-else statement structure and bullet Point 1.3
+_RANDOM_STRING = random.choice(data)
+
+if _RANDOM_STRING in data:
+    print("Yes")
+else:
+    print("No")
+
+#   Concept 2 : range(x)
+#   The above statement specifies a progression from "0" to "x-1", 1 step at a time.
+#   Remember counting from "0" to "x-1" is the same as counting from 1 to x. As in 0, 1, 2, 3 and 1, 2, 3, 4
+#   have the same number of elements.
+
+#   Problem 7). Print range(4)
+print(range(4))
+#   This just specifies a progression from "0" to "3"
+
+#   Concept 3 : for-loop:
+#   for i in range(x>):
+#       print(i)
+#   This statements iterated "i" from "0" to "x-1" and it will print all the values which "i" will take.
+
+#   Problem 8). Using for-loop print numbers from 0 to 3.
+#   Hint: Use concept 2 and 3
+
+for i in range(4):
+    print(i)
+
+#   Problem 9). Print all the elements contained in the variable "data".
+#   Hint: use concept 2 and 3, data_length and <list[i]>
+
+for i in range(data_length):
+    print(data[i])
+
+#   Concept 4: List iteration
+#   for i in <list>:
+#       print(i)
+#   "i" iterates over every element of the list because list is iterable (FACT)
+
+#   Problem 10). Print all the elements of the variable "data" using the iteration method.
+#   Hint: Use concept 4
+
+print("------") #   Ignore
+
+for i in data:
+    print(i)
